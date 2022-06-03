2d5d3589516c31441314d38b72962b572baceb19
averyU1 <criss_19992016@outlook.com>
2022-06-02 21:41:11 -0500

    Se agregan nuevos estilos a las secciones

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 let icon = document.querySelector(".icon");
+let navBar = document.querySelector(".nav-bar-items");
 icon.addEventListener("click", e => {
   let target = e.target;
   target.classList.toggle("active");
+  navBar.classList.toggle("menu-active");
 });
